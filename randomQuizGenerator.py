5e6cc00c75d0a0619d43d628d54262982c472d07
vincent.yook <vincent.yook@PHIL-VINCENTYW7>
2016-11-30 14:54:06 -0500

    Random Quiz Gen

---
+++ randomQuizGenerator.py
@@ -0,0 +1,49 @@
+import random
+
+capitals = {'Alabama': 'Montgomery', 'Alaska': 'Juneau', 'Arizona': 'Phoenix',
+   'Arkansas': 'Little Rock', 'California': 'Sacramento', 'Colorado': 'Denver',
+   'Connecticut': 'Hartford', 'Delaware': 'Dover', 'Florida': 'Tallahassee',
+   'Georgia': 'Atlanta', 'Hawaii': 'Honolulu', 'Idaho': 'Boise', 'Illinois':
+   'Springfield', 'Indiana': 'Indianapolis', 'Iowa': 'Des Moines', 'Kansas':
+   'Topeka', 'Kentucky': 'Frankfort', 'Louisiana': 'Baton Rouge', 'Maine':
+   'Augusta', 'Maryland': 'Annapolis', 'Massachusetts': 'Boston', 'Michigan':
+   'Lansing', 'Minnesota': 'Saint Paul', 'Mississippi': 'Jackson', 'Missouri':
+   'Jefferson City', 'Montana': 'Helena', 'Nebraska': 'Lincoln', 'Nevada':
+   'Carson City', 'New Hampshire': 'Concord', 'New Jersey': 'Trenton', 'New \
+   Mexico': 'Santa Fe', 'New York': 'Albany', 'North Carolina': 'Raleigh',
+   'North Dakota': 'Bismarck', 'Ohio': 'Columbus', 'Oklahoma': 'Oklahoma City',
+   'Oregon': 'Salem', 'Pennsylvania': 'Harrisburg', 'Rhode Island': 'Providence',
+   'South Carolina': 'Columbia', 'South Dakota': 'Pierre', 'Tennessee':
+   'Nashville', 'Texas': 'Austin', 'Utah': 'Salt Lake City', 'Vermont':
+   'Montpelier', 'Virginia': 'Richmond', 'Washington': 'Olympia', 'West \
+   Virginia': 'Charleston', 'Wisconsin': 'Madison', 'Wyoming': 'Cheyenne'}
+
+for quizNum in range(35):
+    #create the quiz and answer key files.
+    quizFile = open('/quiz/quiz'+str(quizNum+1)+'.txt','w')
+    keyFile = open('/quiz/key/key'+str(quizNum+1)+'.txt', 'w')
+    #write out the header for the quiz.
+    quizFile.write('This is quiz ' + str(quizNum+1) + '\n\n')
+    #shuffle the order of the states.
+    capkeys=list(capitals.keys())
+    random.shuffle(capkeys)
+    #write questions
+    for questionNumber in range(50):
+        quizFile.write(str(quizNum+1) + '. What is the capital of ' + capkeys[questionNumber] + '\n')
+        wrongAnswers=list(capitals.values())
+        correctAnswer = capitals[capkeys[questionNumber]]
+        random.shuffle(wrongAnswers)
+        del wrongAnswers[wrongAnswers.index(correctAnswer)]
+        wrongAnswers=random.sample(wrongAnswers,3)
+        answerOptions = wrongAnswers + [correctAnswer]
+        random.shuffle(answerOptions)
+        for option in range(4):
+            quizFile.write(' %s. %s\n' % ('ABCD'[option], answerOptions[option]))
+        quizFile.write('\n')
+        
+        keyFile.write(str(questionNumber + 1) + '. %s' % ('ABCD'[option]))
+        quizFile.close
+        keyFile.close
+
+  
+       
