412694ff854cf546d702c3fc17b2b01ab8db212c
ymyook <youngm.yook@gmail.com>
2016-12-18 21:45:19 -0500

    complete

--- webscraping.py
+++ webscraping.py
@@ -1,1 +1,25 @@
+import webbrowser
+from Tkinter import *
+
+master = Tk()
+e=Entry(master)
+e.pack()
+
+e.focus_set()
+
+temp=''
+def callback():
+	temp=e.get()
+	
+
+b = Button(master, text = "OK", width = 10, command = callback)
+b.pack()
+
+mainloop()
+
+print(temp)
+#urlpath = 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords='+temp
+
+
+#webbrowser.open(urlpath)
 
