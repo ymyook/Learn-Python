3b7957833d3b255f1f871766e630b344761d2d55
ymyook <youngm.yook@gmail.com>
2016-12-18 21:52:02 -0500

    complete

--- webscraping.py
+++ webscraping.py
@@ -7,19 +7,17 @@ e.pack()
 
 e.focus_set()
 
-temp=''
 def callback():
+	global temp
 	temp=e.get()
-	
+	master.quit()	
 
 b = Button(master, text = "OK", width = 10, command = callback)
 b.pack()
 
 mainloop()
 
-print(temp)
-#urlpath = 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords='+temp
+url='https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords='+temp
 
-
-#webbrowser.open(urlpath)
+webbrowser.open(url)
 
