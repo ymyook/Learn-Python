5a64c14dc93681e4e62369ef29188c4624e9478c
ymyook <youngm.yook@gmail.com>
2016-12-21 22:31:37 -0500

    fun night

---
+++ downloadXKCD.py
@@ -0,0 +1,18 @@
+# downloadxkcd.py - downloads xkcd comics
+
+import os, requests, bs4
+
+url='http://www.xkcd.com
+os.makedirs('xkcd', exist_ok=True)
+while not url.endswith('#'):
+    # TODO: Download the page.
+
+    # TODO: Find the URL of the comic image.
+
+    # TODO: Download the image.
+
+    # TODO: Save the image to ./xkcd.
+
+    # TODO: Get the Prev button's url.
+
+print('Done.')
---
+++ setup.py
@@ -0,0 +1,5 @@
+from setuptools import setup
+setup(
+	app=["webscraping.py"],
+	setup_requires=["py2app"],
+)
